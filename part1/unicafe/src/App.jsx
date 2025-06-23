3fa2ac511a5acca5129a0e4e555119e91b9df7ae
romangolev <rv.golev@gmail.com>
2025-06-23 14:51:45 +0200

    feat: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,17 +5,21 @@ const Statistics = (props) => {
 	const all = good + neutral + bad
 	const average = (good - bad)/all
 	const positive = good/all*100
-	return (
-		<>
-			<h1>statistics</h1>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
-			<p>all {all}</p>
-			<p>average {average}</p>
-			<p>positive {positive} %</p>
-		</>
-	)
+	if (good === 0 && neutral ===0 && bad ===0){
+		return (<p>No feedback given</p>)	
+	} else {
+		return (
+			<>
+				<h1>statistics</h1>
+				<p>good {good}</p>
+				<p>neutral {neutral}</p>
+				<p>bad {bad}</p>
+				<p>all {all}</p>
+				<p>average {average}</p>
+				<p>positive {positive} %</p>
+			</>
+		)
+	}
 }
 
 const App = () => {
