4a8eb29fc329304f3c5ebe14309066c33bb5144e
romangolev <rv.golev@gmail.com>
2025-06-25 11:43:43 +0200

    feat: anecdotes step2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
-  const anecdotes = [
+	const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -10,23 +10,29 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
+	]
    
-  const [selected, setSelected] = useState(0)
-  const handleClick = () =>{
-
-	setSelected(Math.floor(Math.random() * 8))
-  }
-  return (
+	const [selected, setSelected] = useState(0)
+	const [count, setCount] = useState(Array(8).fill(0))
+	const handleNextClick = () =>{
+		setSelected(Math.floor(Math.random() * 8))
+	}
+	const handleVoteClick = () =>{
+		const copy = [...count]
+		copy[selected] += 1
+		setCount(copy)
+	}
+	return (
     <>
 		<div>
-			{anecdotes[selected]}
+			<p>{anecdotes[selected]}</p>
+			<p>has {count[selected]} votes</p>
 		</div>
 		<div>
-			<button onClick={handleClick}>next anecdote</button>
+			<button onClick={handleVoteClick}>vote</button>
+			<button onClick={handleNextClick}>next anecdote</button>
 		</div>
-	</>
-  )
+	</>)
 }
 
 export default App
