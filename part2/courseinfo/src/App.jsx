d7b1bb0aec46d14d2a190dec31a0af3dd0f36f99
Roman Golev <rv.golev@gmail.com>
2025-06-29 13:14:38 +0200

    feat: part2 step9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,37 +1,56 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+	const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+	return (courses.map(course =>  <Course key={course.id} course={course} />))
 }
 
 const Course = (props) => {
 	return (
 	<div>
+		<h1>Web development curriculum</h1>
 		<Header course={props.course} />
 		<Content course={props.course} />
 		<Total course={props.course} />
@@ -57,7 +76,7 @@ const Part = (props) => {
 
 const Header = (props) => {
 	return (
-      <h1>{props.course.name}</h1>
+      <h2>{props.course.name}</h2>
 	)
 }
 
