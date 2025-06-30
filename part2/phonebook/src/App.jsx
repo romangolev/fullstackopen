fff92256a957edf8a67db36223fa95ac6e541b98
Roman Golev <rv.golev@gmail.com>
2025-06-30 13:58:48 +0200

    feat: phonebook step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,10 +4,12 @@ const App = () => {
 	const [persons, setPersons] = useState([
 		{
 			name: 'Arto Hellas' ,
+			number: '8 800 800 00 00 ',
 			id: 1
 		}
 	]) 
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
 	
 	const checkName = () => {
 		const find = persons.find(person => person.name === newName)
@@ -22,28 +24,35 @@ const App = () => {
 		} else {
 			const newObj = {
 				name: newName,
+				number: newNumber,
 				id: String(persons.length + 1)
 			}
 			setPersons(persons.concat(newObj))
 			setNewName('')
+			setNewNumber('')
 		}
 	}
-	const handleNoteChange = (event) => {
+
+	const handleNameChange = (event) => {
 		setNewName(event.target.value)
 	}
+
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value)
+	}
+
 	return (
 	<div>
 		<h2>Phonebook</h2>
 		<form onSubmit={addNote}>
-			<div>
-			name: <input value={newName} onChange={handleNoteChange} />
-			</div>
+			<div>name: <input value={newName} onChange={handleNameChange} /></div>
+			<div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
 			<div>
 				<button type="submit">add</button>
 			</div>
 		</form>
 		<h2>Numbers</h2>
-		{persons.map(person => <p>{person.name}</p> )}
+		{persons.map(person => <p>{person.name} {person.number}</p> )}
 	</div>
 	)
 }
