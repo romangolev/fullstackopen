d40d3e45b5ebe0c9a6d4bc08c9888abd93b8f452
Roman Golev <rv.golev@gmail.com>
2025-07-01 13:54:23 +0200

    part2: 2.11 step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-
-const DisplayPhonebook = (props) => {
+const Persons = (props) => {
 	const filtered = props.persons.filter(person => 
 		person.name.toLowerCase().includes(props.search.toLowerCase()))
 	
 	return(
 		<>
-			<h2>Numbers</h2>
 			{filtered.map(person => <p>{person.name} {person.number}</p> )}
 		</>
 	) 
 }
 
+const Filter = (props) => {
+	return (
+		<form>
+			<div>filter shown with <input value={props.search} onChange={props.onSearchChange} />
+		</div>
+		</form>
+	)
+}
+
+const PersonForm = (props) => {
+	
+	return (
+		<form onSubmit={props.addNote}>
+			<div>name: <input value={props.name} onChange={props.onNameChange} /></div>
+			<div>number: <input value={props.number} onChange={props.onNumberChange} /></div>
+			<div>
+				<button type="submit">add</button>
+			</div>
+		</form>
+	)
+}
+
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [newSearch, setNewSearch] = useState('')
@@ -58,21 +74,27 @@ const App = () => {
 		setNewSearch(event.target.value)
 	}
 
+	useEffect(() => {
+		console.log('effect')
+		axios.get('http://localhost:3001/persons')
+			.then(response => {
+				console.log('promise fullfilled')
+				setPersons(response.data)
+			})
+	}, [])
+
 	return (
 	<div>
 		<h2>Phonebook</h2>
-		<form>
-			<div>filter shown with <input value={newSearch} onChange={handleSearchChange} /> </div>
-		</form>
-		<h2>Add a new</h2>
-		<form onSubmit={addNote}>
-			<div>name: <input value={newName} onChange={handleNameChange} /></div>
-			<div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-			<div>
-				<button type="submit">add</button>
-			</div>
-		</form>
-		<DisplayPhonebook persons={persons} search={newSearch} />
+		<Filter search={newSearch} onSearchChange={handleSearchChange} />
+		<h3>Add a new</h3>
+		<PersonForm name={newName}
+					number={newNumber}
+					onNameChange={handleNameChange}
+					onNumberChange={handleNumberChange}
+					addNote={addNote}/>
+		<h3>Numbers</h3>
+		<Persons persons={persons} search={newSearch} />
 	</div>
 	)
 }
