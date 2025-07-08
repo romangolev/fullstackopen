ef3ea8256662376187dfc0b6b86124c0ec8dc3eb
Roman Golev <rv.golev@gmail.com>
2025-07-08 12:26:15 +0200

    ex3.3: step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -32,6 +32,17 @@ app.get('/api/persons', (request, response) => {
 	response.json(persons)
 })
 
+app.get('/api/persons/:uid', (request, response) => {
+	const elem = persons.filter(el => el.id === request.params.uid)
+	if (elem.length){
+		response.json(elem)
+	} else {
+		return response.status(400).send({
+			message: 'Contact with this id does not exist'
+		})
+	}
+})
+
 app.get('/info', (request, response) => {
 	response.send(`Phonebook has info for ${persons.length} people </br> ${new Date()}`)
 })
