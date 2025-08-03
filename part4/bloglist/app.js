10739c60a8ba1e6059be6106f34f788347da57a7
Roman Golev <rv.golev@gmail.com>
2025-08-03 20:12:43 +0200

    ex4.2: bloglist, step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,12 @@
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const express = require('express')
+const blogsRouter = require('./controllers/blogs')
+const app = express()
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+mongoose.connect(config.MONGODB_URI)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,38 +1,8 @@
+const app = require('./app')
+const logger = require('./utils/logger')
 const config = require('./utils/config')
-
-const express = require('express')
-const mongoose = require('mongoose')
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 const PORT = config.PORT 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	logger.info(`Server running on port ${config.PORT}`)
 })
