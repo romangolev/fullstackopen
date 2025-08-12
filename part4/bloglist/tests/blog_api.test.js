d2346948aa73c66d58081b51558b57e3e205abf6
Roman Golev <rv.golev@gmail.com>
2025-08-12 19:33:03 +0200

    ex4.9: blog list tests, step 2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -7,10 +7,23 @@ const assert = require('node:assert')
 const api = supertest(app)
 
 test.only('correct amount of blogs returned', async () => {
-  const res = await api
-    .get('/api/blogs')
-	console.log(res.body)
+	const res = await api
+		.get('/api/blogs')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
 	assert.strictEqual(res.body.length, 2)
+})
+
+test.only('verify that blog has unique identifier property', async () => {
+	const res = await api
+		.get('/api/blogs')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+
+	res.body.forEach(blog => {
+		assert.ok(blog.id, 'blog should have an id property');
+		assert.strictEqual(blog._id, undefined, 'internal _id for blogshould be undefined');
+	})
 })
 
 after(async () => {
