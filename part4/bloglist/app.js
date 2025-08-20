553f43b28caaa1f014984433d8447d2dd0015e0a
Roman Golev <rv.golev@gmail.com>
2025-08-20 23:38:11 +0200

    ex4.15: blog list expansion, step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,10 +2,12 @@ const config = require('./utils/config')
 const mongoose = require('mongoose')
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const app = express()
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 mongoose.connect(config.MONGODB_URI)
 
---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+	const users = await User.find({})
+	console.log('dfdgfgfghf')
+	console.log(users)
+	console.log('Found users:', users.length)
+	response.json(users)
+})
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+
+  } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    return response.status(400).json({ error: 'expected `username` to be unique' })
+  }
+
+  next(error)
+}
+
+module.exports = usersRouter
