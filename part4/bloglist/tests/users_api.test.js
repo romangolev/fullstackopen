2f6d028180e759a050ad330c8a9b47bd1c2bbcba
Roman Golev <rv.golev@gmail.com>
2025-08-27 23:48:27 +0200

    minor fixes

--- part4/bloglist/tests/users_api.test.js
+++ part4/bloglist/tests/users_api.test.js
@@ -13,7 +13,7 @@ describe('when there is initially one user in db', () => {
 	beforeEach(async () => {
 		await User.deleteMany({})
 
-		const passwordHash = await bcrypt.hash('sekret', 10)
+		const passwordHash = await bcrypt.hash('usesrpass', 10)
 		const user = new User({ username: 'root', passwordHash })
 
 		await user.save()
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,3 @@
-
 const errorHandler = (error, request, response, next) => {
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' })
