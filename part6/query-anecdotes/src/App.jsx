cdb66865338c1e02d4e05e7a046349709dec2932
Roman Golev <rv.golev@gmail.com>
2025-10-29 00:13:40 +0100

    ex6.22

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,19 +1,27 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
-
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
     retry: false
   })
  
+  const voteAnecdoteMutation = useMutation({
+      mutationFn: updateAnecdote,
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      }
+  })
+
+  const handleVote = (anecdote) => {
+      const updatedAnecdote = { ...anecdote, votes: anecdote.votes + 1 }
+      voteAnecdoteMutation.mutate(updatedAnecdote)
+  }
+
   console.log(JSON.parse(JSON.stringify(result)))
  
   if (result.isLoading) {
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -22,3 +22,16 @@ export const createAnecdote = async (newAnecdote) => {
     return await response.json()
 }
 
+export const updateAnecdote = async (updatedAnecdote) => {
+    const response = await fetch(`${baseUrl}/${updatedAnecdote.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedAnecdote)
+    })
+
+    if (!response.ok) {
+        throw new Error('Failed to update anecdote')
+    }
+
+    return await response.json()
+}
