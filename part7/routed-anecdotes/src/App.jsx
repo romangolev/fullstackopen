a97340eb314c3c29560f51d72e5f7260325435a9
Roman Golev <rv.golev@gmail.com>
2025-11-10 21:48:18 +0100

    ex7.5: Anecdotes and Hooks step 2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useField } from './hooks'
 import {
   BrowserRouter as Router,
   Routes, Route, Link,
@@ -6,6 +7,7 @@ import {
 } from 'react-router-dom'
 import AnecdoteList from './components/AnecdoteList'
 import Anecdote from './components/Anecdote'
+import CreateNew from './components/CreateNew'
 
 const Menu = () => {
     const padding = { paddingRight: 5 }
@@ -39,53 +41,6 @@ const Footer = () => (
     See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
   </div>
 )
-
-const CreateNew = ({ addNew, setNotification }) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-  const navigate = useNavigate()
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const newAnecdote = {
-      content,
-      author,
-      info,
-      votes: 0
-    }
-    addNew(newAnecdote)
-
-    setNotification(`a new anecdote '${content}' created!`)
-
-    setTimeout(() => {
-      setNotification('')
-    }, 5000)
-
-    navigate('/')
-  }
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
---
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,56 @@
+import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
+
+const CreateNew = ({ addNew, setNotification }) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+
+    setNotification(`a new anecdote '${content.value}' created!`)
+    setTimeout(() => setNotification(''), 5000)
+    navigate('/')
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+  const stripReset = ({ reset, ...props }) => props
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...stripReset(content)} />
+        </div>
+        <div>
+          author
+          <input {...stripReset(author)} />
+        </div>
+        <div>
+          url for more info
+          <input {...stripReset(info)} />
+        </div>
+        <button>create</button>
+        <button onClick={handleReset}>reset</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
