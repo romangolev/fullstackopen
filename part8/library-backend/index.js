a86d89b82daf808dfa87ecfc38349cdfc844c074
Roman Golev <rv.golev@gmail.com>
2025-12-19 23:39:53 +0100

    8.3: All authors

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -27,20 +27,6 @@ let authors = [
   },
 ]
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
-
 let books = [
   {
     title: 'Clean Code',
@@ -93,29 +79,27 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = `
   type Author {
     name: String!
     id: ID!
-    born: String!
+    born: Int
+    bookCount: Int!
   }
 
   type Book {
     title: String!
-    published: String!
+    published: Int!
     author: String!
     id: ID!
-    genres: [String!]
+    genres: [String!]!
   }
 
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks: [Book!]
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -123,7 +107,17 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => {
+      const bookCountByAuthor = books.reduce((acc, b) => {
+        acc[b.author] = (acc[b.author] || 0) + 1
+        return acc
+      }, {})
+      return authors.map((a) => ({
+        ...a,
+        bookCount: bookCountByAuthor[a.name] || 0,
+      }))
+    }
   }
 }
 
