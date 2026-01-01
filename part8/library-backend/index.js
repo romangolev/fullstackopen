e3b81351bdb8ad8bab00f1befeee09a7fb2910df
Roman Golev <rv.golev@gmail.com>
2026-01-01 23:07:13 +0100

    ex8.5: books by genre

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,10 +108,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
+      let result = books
+      if (args.author) {
+        result = result.filter((b) => b.author === args.author)
       }
-      return books.filter((b) => b.author === args.author)
+      if (args.genre) {
+        result = result.filter((b) => b.genres.includes(args.genre))
+      }
+      return result
     },
     allAuthors: () => {
       const bookCountByAuthor = books.reduce((acc, b) => {
