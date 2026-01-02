dd9272ae24a48c66e82029e0f4fba9105a0cd030
Roman Golev <rv.golev@gmail.com>
2026-01-02 20:44:31 +0100

    ex8.7: updating the birth year of author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -110,6 +110,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book!
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -153,7 +157,19 @@ const resolvers = {
       }
       books = books.concat(newBook)
       return newBook
-    } 
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = {
+        ...author,
+        born: args.setBornTo,
+      }
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
+    }
   }
 }
 
