9c5b2c16ba0dec645ecaba19d855df4203dbb690
Roman Golev <rv.golev@gmail.com>
2026-01-03 14:50:12 +0100

    ex8.8: authors view

---
+++ part8/library-frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+
+const ALL_AUTHORS = gql`
+  query AllAuthors {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
+const App = () => {
+  const [page, setPage] = useState('authors')
+
+  const allAuthors = useQuery(ALL_AUTHORS)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+        loading={allAuthors.loading}
+        authors={allAuthors.data ? allAuthors.data.allAuthors : []}
+      />
+
+      <Books show={page === 'books'} />
+
+      <NewBook show={page === 'add'} />
+    </div>
+  )
+}
+
+export default App
---
+++ part8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,33 @@
+const Authors = ({ show, loading, authors }) => {
+  if (!show) {
+    return null
+  }
+
+  if (loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
