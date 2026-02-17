f588e895baa147c5d569af7b3c7d34236cd18e4f
Roman Golev <rv.golev@gmail.com>
2026-02-17 22:00:59 +0100

    ex9.1: body mass index

---
+++ part9/calc-ts/bmiCalculator.ts
@@ -0,0 +1,24 @@
+
+const calculateBmi = (height: number, weight: number): string => {
+     const bmi = weight / ((height / 100) ** 2)
+     switch (true) {
+          case bmi < 16:
+               return 'Underweight (Severe thinness)'
+          case bmi >= 16 && bmi < 17:
+               return 'Underweight (Moderate thinness)'
+          case bmi >= 17 && bmi < 18.5:
+               return 'Underweight (Mild thinness)'
+          case bmi >= 18.5 && bmi < 25:
+               return 'Normal range'
+          case bmi >= 25 && bmi < 30:
+               return 'Overweight (Pre-obese)'
+          case bmi >= 30 && bmi < 35:
+               return 'Obese (Class I)'
+          case bmi >= 35 && bmi < 40:
+               return 'Obese (Class II)'
+          default:
+               return 'Obese (Class III)'
+     }
+}
+
+console.log(calculateBmi(180, 74))
