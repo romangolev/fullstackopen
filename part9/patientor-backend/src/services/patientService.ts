b7ab56aee7cc557309f5e32647da1e6ef654ff25
Roman Golev <rv.golev@gmail.com>
2026-04-15 16:00:00 +0200

    ex9.11: Patientor backend, step4

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,23 @@
+import patientData from '../../data/patients';
+import { Patient, NonSensitivePatient } from '../types';
+
+const patients: Patient[] = patientData;
+
+const getEntries = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getEntries,
+  getNonSensitiveEntries,
+};
